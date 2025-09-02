e5b8fd84153a4a9ab439f3a2621efe479c79229e
matanya alkobi <matanor123456@gmail.com>
2025-09-02 14:54:45 +0300

    Creating a home page with three buttons

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+export default function HomePage(){
+    return(
+        <>
+        <div className="home-page">
+            <button>play</button>
+            <button>login</button>
+            <button>leader board</button>
+        </div>
+        </>
+    )
+}
